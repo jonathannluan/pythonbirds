1f050378976b5ddf3af732e4ba9005aa116823f9
Jonathan Luan <jonathanluanalvesbiserra@gmail.com>
2022-03-07 11:14:37 -0300

    Criado instâncias nome e idade

--- oo/Pessoa.py
+++ oo/Pessoa.py
@@ -1,9 +1,13 @@
 class Pessoa:
+    def __init__(self,nome=None,idade=35):
+        self.idade = idade
+        self.nome=nome
     def cumprimentar(self):
         return f'Olá'
 
 
 if __name__ == '__main__':
-    p=Pessoa()
-
-print(p.cumprimentar())
+    p= Pessoa('Luan')
+    print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
